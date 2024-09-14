d787135c9b3af05bb90df114aacf0e71cc75c58c
Yosuke-Shibata <107012976+Yosuke-Shibata@users.noreply.github.com>
2024-09-14 16:18:42 +0900

    Init commit.

---
+++ src/test/kotlin/yoshibata/exam/quocard/libraryApp/LibraryAppApplicationTests.kt
@@ -0,0 +1,10 @@
+package yoshibata.exam.quocard.libraryApp
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryAppApplicationTests {
+  // 	@Test
+// 	fun contextLoads() {
+// 	}
+}
