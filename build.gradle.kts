1eff47b42d5f9984bf8570c111b3b65c8f08812f
Yosuke-Shibata <107012976+Yosuke-Shibata@users.noreply.github.com>
2024-09-15 16:55:44 +0900

    JOOQでのコード生成まで

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
   kotlin("plugin.spring") version "1.9.25"
   id("org.springframework.boot") version "3.3.3"
   id("io.spring.dependency-management") version "1.1.6"
+  id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+  id("org.jooq.jooq-codegen-gradle") version "3.19.11"
 }
 
 group = "yoshibata.exam.quocard"
@@ -29,6 +31,10 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jooq:jooq:3.19.11")
+  implementation("org.jooq:jooq-meta:3.19.11")
+  implementation("org.jooq:jooq-codegen:3.19.11")
+  implementation("org.jooq:jooq-postgres-extensions:3.19.11")
   compileOnly("org.projectlombok:lombok")
   developmentOnly("org.springframework.boot:spring-boot-docker-compose")
   runtimeOnly("org.postgresql:postgresql")
@@ -36,6 +42,7 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  jooqCodegen("org.postgresql:postgresql")
 }
 
 kotlin {
@@ -46,4 +53,25 @@ kotlin {
 
 tasks.withType<Test> {
   useJUnitPlatform()
+}
+
+jooq {
+  configuration {
+    jdbc {
+      driver = "org.postgresql.Driver"
+      url = "jdbc:postgresql://localhost:5432/libraryappdb"
+      user = "library_app"
+      password = "456789"
+    }
+    generator {
+      database {
+        name = "org.jooq.meta.postgres.PostgresDatabase"
+        inputSchema = "library_app_schema"
+      }
+      target {
+        packageName = "yoshibata.exam.quocard.libraryApp.JOOQ"
+        directory = "src/main/kotlin"
+      }
+    }
+  }
 }
